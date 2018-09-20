eda692c400cfaa8f1f267ed5b38318cba67478af
eliya <eliyaai@yaomaiche.com>
2018-09-20 16:43:48 +0800

    完成轮播图效果

--- src/views/main.vue
+++ src/views/main.vue
@@ -5,7 +5,9 @@
     <template>
       <div class="swiper-container">
         <div class="swiper-wrapper">
-          <div class="swiper-slide img_banner" v-for="imgUrl in listImg"  :key="imgUrl" :style="{backgroundImage:'url(' + imgUrl + ')' }"></div>
+          <div class="swiper-slide" v-for="imgUrl in listImg" :key="imgUrl">
+            <img :src="imgUrl">
+          </div>
         </div>
         <div class="swiper-pagination swiper-pagination-white"></div>
       </div>
@@ -21,55 +23,50 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      listImg: [require('../images/brand_detail/01.png'),require('../images/brand_detail/02.png')]
+      listImg: [require('../images/brand_detail/01.png'),
+        require('../images/brand_detail/02.png')]
     }
   },
   mounted () {
     setTimeout(function () {
       var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        },
+        autoplay: true,
+        delay: 3000,
         loop: true,
-        speed: 600,
-        autoplay: 4000,
+        autoHeight: true,
         onTouchEnd: function () {
           swiper.startAutoplay()
         }
       })
-    }, 100)
+    }, 1000)
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .img_banner{
-    width: 100%;
-    height: auto;
-  }
   .swiper-container {
     width: 100%;
-    height: 10rem;
+    min-height: 100px;
     .swiper-wrapper {
       width: 100%;
       height: 100%;
     }
     .swiper-slide {
+      overflow: hidden;
       background-position: center;
       background-size: cover;
       width: 100%;
       height: 100%;
-    img {
-      width: 100%;
-      height: 100%;
+      img {
+        width: 100%;
+        height: 100%;
+      }
     }
-  }
-  .swiper-pagination-bullet {
-    width:0.833rem;
-    height: 0.833rem;
-    display: inline-block;
-    background: #7c5e53;
-  }
   }
 h1, h2 {
   font-weight: normal;
