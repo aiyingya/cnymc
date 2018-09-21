754bf57e931e4204436977fd2d89a84802224096
eliya <eliyaai@yaomaiche.com>
2018-09-21 16:17:30 +0800

    提交首页icon的效果

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,7 +1,6 @@
 <!--首页-->
 <template>
   <div class="hello">
-
     <template>
       <div class="swiper-container">
         <div class="swiper-wrapper">
@@ -12,6 +11,14 @@
         <div class="swiper-pagination swiper-pagination-white"></div>
       </div>
     </template>
+    <div class="main_icons">
+        <ul>
+          <li><div><img src="../images/main/icon_about_yuntu.png"></div><div>关于运图</div></li>
+          <li><div><img src="../images/main/icon_brand_introduce.png"></div><div>关于运图</div></li>
+          <li><div><img src="../images/main/icon_dynamic.png"></div><div>关于运图</div></li>
+          <li><div><img src="../images/main/icon_store_list.png"></div><div>关于运图</div></li>
+        </ul>
+    </div>
   </div>
 </template>
 
@@ -46,6 +53,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
   .swiper-container {
     width: 100%;
     min-height: 100px;
@@ -65,6 +73,37 @@ export default {
       }
     }
   }
+
+  .main_icons{
+    height: 93px;
+    background-color: #FFFFFF;
+  }
+  .main_icons ul{
+    display: -webkit-box;
+    height: 100%;
+  }
+  .main_icons li{
+    display: -webkit-box;
+     -webkit-box-flex: 1;
+    -webkit-box-align: center;
+    -webkit-box-pack: center;
+    -webkit-box-orient: vertical;
+  }
+  .main_icons li>div{
+    -webkit-box-align: center;
+    -webkit-box-pack: center;
+    color: #4C4B4B;
+    font-size: 12px;
+  }
+  .main_icons li>div:first-child{
+    margin-bottom: 5px;
+  }
+  .main_icons li>div img{
+    height: 42px;
+    width: 42px;
+  }
+
+
 h1, h2 {
   font-weight: normal;
 }
