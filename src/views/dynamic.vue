27ae2144cf171bc35925dbc4a13f878b4c1bf8ab
eliya <eliyaai@yaomaiche.com>
2018-09-28 10:13:41 +0800

    数据获取动态列表

--- src/views/dynamic.vue
+++ src/views/dynamic.vue
@@ -5,70 +5,59 @@
       <img src="../images/dynamic/banner.png">
       <img class="img_title" src="../images/dynamic/banner_title.png">
     </div>
-    <div class="dynamic_item">
+    <div v-for="item in dynamicList" :key="item.index" class="dynamic_item">
       <div class="dynamic_detial">
-        <h4>新店开业成交23台，遂川店开业庆典&吉安区团购大联动</h4>
-        <div>2018-09-08 11:14:53</div>
+        <h4>{{item.title}}</h4>
+        <div>{{item.time}}</div>
       </div>
-      <img src="../images/dynamic/01/image001.jpg">
+      <img :src="item.imgSrc">
     </div>
-    <div class="dynamic_item">
-      <div class="dynamic_detial">
-        <h4>盛夏购车季，运图要买车信丰店团购活动圆满结束！</h4>
-        <div>2018-09-03 11:23:02</div>
-      </div>
-      <img src="../images/dynamic/02/image001.jpg">
-    </div>
-    <div class="dynamic_item">
-      <div class="dynamic_detial">
-        <h4>前方高能，运图要买车要搞大事情啦</h4>
-        <div>2018-09-03 11:23:02</div>
-      </div>
-      <img src="../images/dynamic/03.png">
-    </div>
-    <div class="dynamic_item">
-      <div class="dynamic_detial">
-        <h4>服务无止境，运图要买车“客户满意月”活动圆满结束</h4>
-        <div>2018-06-18 11:08:02</div>
-      </div>
-      <img src="../images/dynamic/04/image001.jpg">
-    </div>
-    <div class="dynamic_item">
-      <div class="dynamic_detial">
-        <h4>全囯连锁汽车超市，运图要买车强势登陆安福</h4>
-        <div>2018-04-01 10:05:08</div>
-      </div>
-      <img src="../images/dynamic/05/image001.jpg">
-    </div>
-    <div class="dynamic_item">
-      <div class="dynamic_detial">
-        <h4>热情如火！运图要买车开春团车会圆满结束</h4>
-        <div>2018-03-26 11:08:02</div>
-      </div>
-      <img src="../images/dynamic/06/image001.jpg">
-    </div>
-    <div class="dynamic_item">
-      <div class="dynamic_detial">
-        <h4>运图要买车全程助力，大余山地马拉松圆满落幕</h4>
-        <div>2018-04-23 11:08:02</div>
-      </div>
-      <img src="../images/dynamic/07/image001.jpg">
-    </div>
+
   </div>
 </template>
 
 <script>
-import dynamic01 from '@/components/dynamic/01.vue'
-import dynamic02 from '@/components/dynamic/02.vue'
 export default {
   name: 'Dynamic',
-  components: {
-    dynamic01: dynamic01,
-    dynamic02: dynamic02
-  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      dynamicList: [{
+        title: '新店开业成交23台，遂川店开业庆典&吉安区团购大联动',
+        time: '2018-09-08 11:14:53',
+        imgSrc: require('../images/dynamic/01/image001.jpg'),
+        index: 1
+      }, {
+        title: '盛夏购车季，运图要买车信丰店团购活动圆满结束！',
+        time: '2018-09-03 11:23:02',
+        imgSrc: require('../images/dynamic/02/image001.jpg'),
+        index: 2
+      }, {
+        title: '前方高能，运图要买车要搞大事情啦',
+        time: '2018-09-03 11:23:02',
+        imgSrc: require('../images/dynamic/03.png'),
+        index: 3
+      }, {
+        title: '服务无止境，运图要买车“客户满意月”活动圆满结束',
+        time: '2018-06-18 11:08:02',
+        imgSrc: require('../images/dynamic/04/image001.jpg'),
+        index: 4
+      }, {
+        title: '全囯连锁汽车超市，运图要买车强势登陆安福',
+        time: '2018-04-01 10:05:08',
+        imgSrc: require('../images/dynamic/05/image001.jpg'),
+        index: 5
+      }, {
+        title: '热情如火！运图要买车开春团车会圆满结束',
+        time: '2018-03-26 11:08:02',
+        imgSrc: require('../images/dynamic/06/image001.jpg'),
+        index: 6
+      }, {
+        title: '运图要买车全程助力，大余山地马拉松圆满落幕',
+        time: '2018-04-23 11:08:02',
+        imgSrc: require('../images/dynamic/07/image001.jpg'),
+        index: 7
+      }]
     }
   }
 }
