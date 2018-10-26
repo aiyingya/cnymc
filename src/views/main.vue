716fe0175394a1faf536377ca73d3d931d881864
eliya <eliyaai@yaomaiche.com>
2018-10-26 15:54:12 +0800

    Less

--- src/views/main.vue
+++ src/views/main.vue
@@ -82,7 +82,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less">
+<style scoped lang="less" type="text/less">
   .swiper-container {
     width: 100%;
     min-height: 100px;
